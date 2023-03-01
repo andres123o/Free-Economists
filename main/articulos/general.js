c0dd837fe55238e8d00134f4f2bb42fea0c652dd
Andres Olaya <acrista04766@universidadean.edu.co>
2023-02-28 21:53:01 -0500

    Fourth

--- main/articulos/general.js
+++ main/articulos/general.js
@@ -2,14 +2,21 @@ let btnAbrir = document.getElementById("abrirVentana");
 let ventanaEmergente = document.getElementById("miVentanaEmergente");
 let btnCerrar = document.getElementById("cerrarVentana")
 let back = document.getElementById("info-article")
+let invst = document.getElementById("investigacion")
+
+invst.addEventListener("click", function(){
+  window.open("/POTENCIAL DE LA UCHUVA EN LOS MERCADOS INTERNACIONALES. (1)/POTENCIAL DE LA UCHUVA EN LOS MERCADOS INTERNACIONALES. (1).pdf")
+})
 
 btnAbrir.addEventListener("click", function() {
-  ventanaEmergente.style.display = "block";
+  // ventanaEmergente.style.display = "block";
   back.style.opacity = 0.1;
+  ventanaEmergente.style.transform = "scale(1)";
 });
 
 btnCerrar.addEventListener("click", function() {
-    ventanaEmergente.style.display = "none"
+    // ventanaEmergente.style.display = "none"
     back.style.opacity = 1;
+    ventanaEmergente.style.transform = "scale(0)"
 })
 
